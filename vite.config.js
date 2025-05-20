1ebab97d44238d8c4c99238d3985eec60890bc16
Gilbert Danso <72208611+kdrighteo@users.noreply.github.com>
2025-05-20 11:39:24 +0000

    new

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
+  server: {
+    port: 3000,
+    open: true
+  }
 }) 
