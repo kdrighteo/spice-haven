e6cbd7bcaeb9304f73cb05ea867d774eba5149c4
Gilbert Danso <72208611+kdrighteo@users.noreply.github.com>
2025-05-20 11:31:46 +0000

    new

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/spice-haven/',
+  base: '/spice-haven-1/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
